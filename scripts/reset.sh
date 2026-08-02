69b3860e6819fadc5ac8520d184fdd6f2d0b4e6b
Akene-Uzezi <xtremejoexj@gmail.com>
2026-08-02 14:09:03 +0100

    add reset script. still not perfect

---
+++ scripts/reset.sh
@@ -0,0 +1,14 @@
+set -e
+
+echo "removing all containers"
+docker rm -f $(docker ps -aq)
+
+echo "removing all volumes"
+docker volume rm $(docker volume ls -q)
+
+read -p "Do you want to restart containers(y/n): " choice
+if ["$choice" = "y"]; then
+  docker compose up
+else
+  echo "restart stopped by user"
+fi
