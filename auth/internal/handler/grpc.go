90ef39546f4b0f99d634ba9319b8815e03533e97
Akene-Uzezi <xtremejoexj@gmail.com>
2026-08-03 09:48:00 +0100

    fix minor typo

--- auth/internal/handler/grpc.go
+++ auth/internal/handler/grpc.go
@@ -41,6 +41,6 @@ func (h *AuthGRPCHanlder) CreateUser(ctx context.Context, req *authpb.CreateUser
 	return response, nil
 }
 
-func (h *AuthGRPCHanlder) Login(ctx context.Context, req *authpb.LoginRequest) (*authpb.LoginRequest, error) {
+func (h *AuthGRPCHanlder) Login(ctx context.Context, req *authpb.LoginRequest) (*authpb.LoginResponse, error) {
 	return nil, nil
 }
