a9a28387298a8cc383720cb8e07ebf9968c844dd
Akene-Uzezi <xtremejoexj@gmail.com>
2026-08-04 10:45:48 +0100

    remove unused types in types.go

--- gateway/internal/handler/types.go
+++ gateway/internal/handler/types.go
@@ -4,18 +4,7 @@ import (
 	orderpb "ecommerce-api/gen/order"
 )
 
-type CreateUserPayload struct {
-	Email    string `json:"email"`
-	Password string `json:"password"`
-	Name     string `json:"name"`
-}
-
 type CreateOrderPayload struct {
 	CustomerID string               `json:"customer_id"`
 	Items      []*orderpb.OrderItem `json:"items"`
-}
-
-type LoginRequest struct {
-	Email    string `json:"email"`
-	Password string `json:"password"`
 }
