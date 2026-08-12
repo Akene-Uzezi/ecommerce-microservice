8ed813b1e9aaff25d5d2323554e55864ca57db53
Akene-Uzezi <xtremejoexj@gmail.com>
2026-08-12 23:29:08 +0100

    start working on adding auth db unit test. add test script

---
+++ auth/internal/db/db_test.go
@@ -0,0 +1,25 @@
+package db
+
+import (
+	shared "ecommerce-shared"
+	"log"
+	"os"
+	"testing"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+var testPool *pgxpool.Pool
+
+func TestMain(m *testing.M) {
+	pool, cleanup, err := shared.SetupTestDBSuite("/scripts/auth_init.sql")
+	if err != nil {
+		log.Fatalf("failed to initialize test container: %v", err)
+	}
+	testPool = pool
+	exitCode := m.Run()
+
+	cleanup()
+
+	os.Exit(exitCode)
+}
