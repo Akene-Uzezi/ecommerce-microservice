1fa76f675a8e5910e734bab21bd637e58e86e6c6
Akene-Uzezi <xtremejoexj@gmail.com>
2026-08-12 23:20:26 +0100

    start working on adding tests

---
+++ shared/db.go
@@ -0,0 +1,90 @@
+package shared
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/testcontainers/testcontainers-go/modules/postgres"
+)
+
+func SetupTestDBSuite(sql string) (*pgxpool.Pool, func(), error) {
+	ctx := context.Background()
+	pgContainer, err := postgres.Run(
+		ctx, "postgres:16-alpine",
+		postgres.WithDatabase("testdb"),
+		postgres.WithUsername("test"),
+		postgres.WithPassword("test"),
+	)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	cleanup := func() {
+		pgContainer.Terminate(ctx)
+	}
+
+	connStr, _ := pgContainer.ConnectionString(ctx, "sslmode=disable")
+	config, err := pgxpool.ParseConfig(connStr)
+	if err != nil {
+		cleanup()
+		return nil, nil, err
+	}
+
+	config.MaxConns = 10
+	config.MinConns = 2
+
+	pool, err := pgxpool.NewWithConfig(ctx, config)
+	if err != nil {
+		cleanup()
+		return nil, nil, err
+	}
+
+	var PingErr error
+	for range 10 {
+		PingErr = pool.Ping(ctx)
+		if PingErr == nil {
+			break
+		}
+		time.Sleep(500 * time.Millisecond)
+	}
+	if PingErr != nil {
+		cleanup()
+		return nil, nil, PingErr
+	}
+
+	dir, err := os.Getwd()
+	if err != nil {
+		cleanup()
+		return nil, nil, err
+	}
+
+	for {
+		if _, err := os.Stat(filepath.Join(dir, "go.work")); err == nil {
+			break
+		}
+		parent := filepath.Dir(dir)
+		if parent == dir {
+			cleanup()
+			return nil, nil, os.ErrNotExist
+		}
+		dir = parent
+	}
+
+	schemaPath := filepath.Join(dir, fmt.Sprint(sql))
+	schema, err := os.ReadFile(schemaPath)
+	if err != nil {
+		cleanup()
+		return nil, nil, err
+	}
+	_, err = pool.Exec(ctx, string(schema))
+	if err != nil {
+		cleanup()
+		return nil, nil, err
+	}
+
+	return pool, cleanup, nil
+}
