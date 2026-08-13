e3ed89c64ce640ae6fed33f3e62390278ceb8a89
Akene-Uzezi <xtremejoexj@gmail.com>
2026-08-13 15:11:55 +0100

    trying to write integration tests with the gateway file

--- gateway/internal/handler/main_test.go
+++ gateway/internal/handler/main_test.go
@@ -2,8 +2,10 @@ package handler
 
 import (
 	shared "ecommerce-shared"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"testing"
 
 	authpb "ecommerce-api/gen/auth"
@@ -12,9 +14,10 @@ import (
 	"google.golang.org/grpc/credentials/insecure"
 )
 
-// gatewayPort     = shared.GetEnvString("GATEWAY_PORT", "3000")
 // orderServiceURL = shared.GetEnvString("ORDER_SERVICE_URL", "localhost:4444")
+
 var (
+	gatewayPort    = shared.GetEnvString("GATEWAY_PORT", "3000")
 	authServiceURL = shared.GetEnvString("AUTH_SERVICE_URL", "localhost:5555")
 	mux            *http.ServeMux
 	authClient     authpb.AuthServiceClient
@@ -29,8 +32,17 @@ func TestMain(m *testing.M) {
 	if err != nil {
 		log.Fatalf("Error creating auth client: %s", err)
 	}
+	addr := fmt.Sprintf(":%s", gatewayPort)
 	mux = http.NewServeMux()
 	authClient = authpb.NewAuthServiceClient(authConn)
 	authHandler = NewAuthHTTPHandler(authClient)
 	authHandler.RegisterRoutes(mux)
+	go func() {
+		log.Printf("server running on port %s", addr)
+		if err := http.ListenAndServe(addr, mux); err != nil {
+			log.Fatalf("unable to start server: %s", err)
+		}
+	}()
+	exitCode := m.Run()
+	os.Exit(exitCode)
 }
