cd79c71a3b887a453fe9be2aedc61ccbb03a00d7
Akene-Uzezi <xtremejoexj@gmail.com>
2026-08-15 11:43:51 +0100

    add database stubs

--- products/internal/db/products.go
+++ products/internal/db/products.go
@@ -1,7 +1,21 @@
 package db
 
-import "github.com/jackc/pgx/v5/pgxpool"
+import (
+	"context"
+
+	productspb "ecommerce-api/gen/products"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
 
 type ProductModel struct {
 	DB *pgxpool.Pool
+}
+
+func (m *ProductModel) AddProduct(ctx context.Context, req *productspb.AddProductRequest) (*productspb.AddProductResponse, error) {
+	return nil, nil
+}
+
+func (m *ProductModel) GetProduct(ctx context.Context, req *productspb.GetProductsRequest) (*productspb.GetProductsResponse, error) {
+	return nil, nil
 }
