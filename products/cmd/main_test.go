c2561796c181b4927069d7062862279c699339e9
Akene-Uzezi <xtremejoexj@gmail.com>
2026-09-15 15:26:59 +0100

    start working on adding test for the product service

---
+++ products/cmd/main_test.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"ecommerce-products/internal/db"
+	"ecommerce-products/internal/handler"
+	shared "ecommerce-shared"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"testing"
+
+	productspb "ecommerce-api/gen/products"
+
+	"google.golang.org/grpc"
+)
+
+func TestMain(m *testing.M) {
+	pool, cleanup, err := shared.SetupTestDBSuite("/scripts/products_init.sql")
+	if err != nil {
+		log.Fatalf("failed to init db: %s", err)
+	}
+	l, err := net.Listen("tcp", fmt.Sprintf(":%s", productsPort))
+	if err != nil {
+		log.Fatal("failed to listen on products port: %s", err)
+	}
+
+	grpcServer := grpc.NewServer()
+	productsHandler := handler.NewProductGRPCHandler(db.NewModels(pool))
+	productspb.RegisterProductServiceServer(grpcServer, productsHandler)
+
+	go func() {
+		log.Printf("products service started on port %s", productsPort)
+		if err := grpcServer.Serve(l); err != nil {
+			log.Printf("grpc server stopped, %s", err)
+		}
+	}()
+
+	exitcode := m.Run()
+	grpcServer.GracefulStop()
+	cleanup()
+	os.Exit(exitcode)
+}
