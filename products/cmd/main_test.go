dd43a355d6ff2e2a9cd27684e7a043fd602d66f1
Akene-Uzezi <xtremejoexj@gmail.com>
2026-09-16 13:51:58 +0100

    add tests for the handler for some sort of integrated test

--- products/cmd/main_test.go
+++ products/cmd/main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"ecommerce-products/internal/db"
 	"ecommerce-products/internal/handler"
 	shared "ecommerce-shared"
@@ -12,8 +13,11 @@ import (
 
 	productspb "ecommerce-api/gen/products"
 
+	"github.com/stretchr/testify/assert"
 	"google.golang.org/grpc"
 )
+
+var productsHandler *handler.ProductGRPCHandler
 
 func TestMain(m *testing.M) {
 	pool, cleanup, err := shared.SetupTestDBSuite("/scripts/products_init.sql")
@@ -26,7 +30,7 @@ func TestMain(m *testing.M) {
 	}
 
 	grpcServer := grpc.NewServer()
-	productsHandler := handler.NewProductGRPCHandler(db.NewModels(pool))
+	productsHandler = handler.NewProductGRPCHandler(db.NewModels(pool))
 	productspb.RegisterProductServiceServer(grpcServer, productsHandler)
 
 	go func() {
@@ -40,4 +44,33 @@ func TestMain(m *testing.M) {
 	grpcServer.GracefulStop()
 	cleanup()
 	os.Exit(exitcode)
+}
+
+func TestAddProductInt(t *testing.T) {
+	ctx := context.Background()
+	req := &productspb.AddProductRequest{
+		Product: &productspb.Product{
+			Name:     "testproduct",
+			Price:    10.32,
+			Quantity: 4,
+		},
+	}
+	_, err := productsHandler.AddProduct(ctx, req)
+	assert.NoError(t, err)
+}
+
+func TestGetProductInt(t *testing.T) {
+	ctx := context.Background()
+	req := &productspb.GetProductRequest{
+		Name: "testproduct",
+	}
+	_, err := productsHandler.GetProduct(ctx, req)
+	assert.NoError(t, err)
+}
+
+func TestGetProductsInt(t *testing.T) {
+	ctx := context.Background()
+	req := &productspb.GetProductsRequest{}
+	_, err := productsHandler.GetProducts(ctx, req)
+	assert.NoError(t, err)
 }
