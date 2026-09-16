b099472cb36f3fbd2bba100ec27d3112f56dbcfa
Akene-Uzezi <xtremejoexj@gmail.com>
2026-09-16 12:11:32 +0100

    add test addProducts function to the test file. working on resolving the segfault.

--- products/internal/db/products_test.go
+++ products/internal/db/products_test.go
@@ -1,10 +1,27 @@
 package db
 
-import "testing"
+import (
+	"context"
+	"testing"
+
+	productspb "ecommerce-api/gen/products"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestFunction(t *testing.T) {
 	t.Log("test func works")
 }
 
 func TestAddProduct(t *testing.T) {
+	ctx := context.Background()
+	req := &productspb.AddProductRequest{
+		Product: &productspb.Product{
+			Name:     "testproduct",
+			Price:    10.32,
+			Quantity: 4,
+		},
+	}
+	_, err := productModel.AddProduct(ctx, req)
+	assert.NoError(t, err)
 }
